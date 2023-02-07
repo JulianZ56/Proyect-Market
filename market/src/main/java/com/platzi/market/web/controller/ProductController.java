b3aa4f4547a39f1423fa4f216b53ff9b3425b026
julian.lozano <julian.lozano@olsoftware.com>
2023-02-06 20:33:21 -0500

    Servicios y controladores

---
+++ market/src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.platzi.market.web.controller;
+
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/all")
+    public List<Product> getAll(){
+        return productService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId){
+        return productService.getProduct(productId);
+    }
+
+    @GetMapping("/category/{id}")
+    public Optional<List<Product>> getByCategory(@PathVariable("id") int categoryId){
+        return productService.getByCategory(categoryId);
+    }
+
+    @GetMapping("/category/Escaso/{id}")
+    public Optional<List<Product>> getScarseProducts(@PathVariable("id") int quantity){
+        return productService.getScarseProducts(quantity);
+    }
+
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product){
+        return productService.save(product);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable("id") int productId){
+        return productService.delete(productId);
+    }
+
+}
